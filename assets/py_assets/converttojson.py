c50598f8daa59a4b102f29ba16eb5d43a7c4b3b6
Adam L. Cairns <adam.cairns13@yahoo.com>
2021-01-29 11:59:07 -0500

    Formatted json files for new structure and debugged errors

--- assets/py_assets/converttojson.py
+++ assets/py_assets/converttojson.py
@@ -1,9 +1,9 @@
 import pandas
 import json
 
-df = pandas.read_csv('./discord_bots/doublelung_resources/species_intents.csv')
+df = pandas.read_csv('./assets/data/csv/weapon.csv')
 
-result = df.to_json('./discord_bots/doublelung_resources/species_intents.csv', orient='records', lines=True)
+result = df.to_json('./lib/db/weapon.json', orient='records', lines=True)
 s1=json.dumps(result)
 parsed = json.loads(s1)
 json.dumps(parsed, indent=4) 
