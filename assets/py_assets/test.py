80ba7baf731de89feb085eea334c7279f39e6e39
Adam L. Cairns <adam.cairns13@yahoo.com>
2021-01-30 21:32:32 -0500

    Data upates from Mac

--- assets/py_assets/test.py
+++ assets/py_assets/test.py
@@ -17,8 +17,11 @@ push_data = []
 with open('./lib/db/stats.json','w') as tf:
     for i in stat_list:
         index = stat_list.index(i)
-        stat_dict = {"statid":i,"description":"DESCRIPTION","inline":False,"group":stat_group[index]}
+        if stat_group[index] == "species":
+            stat_dict = {"statid":i,"description":"DESCRIPTION","inline":False,"cmd_set":stat_group[index],"sort":0,"group":"GROUP"}
+        else:
+            stat_dict = {"statid":i,"description":"DESCRIPTION","inline":False,"cmd_set":stat_group[index],"sort":0,}
         push_data.append(stat_dict)
 
-    final_dict={"stats":push_data}
+    final_dict = {"stats":push_data}
     json.dump(final_dict,tf,indent=4)
